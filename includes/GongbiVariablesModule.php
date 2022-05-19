a95fb8f7d83fa3ea759c54a17ccd4e27cae59ad4
WaitSpring <me@waitspring.com>
2022-05-19 13:23:05 +0800

    add filename

--- includes/GongbiVariablesModule.php
+++ includes/GongbiVariablesModule.php
@@ -1,6 +1,7 @@
 <?php
 /** 
  * Gongbi Skin
+ * includes/GongbiVariablesModule.php
  * © 2015-2021 Isarra
  * © 2021-2022 WaitSpring
  */
--- includes/SkinGongbi.php
+++ includes/SkinGongbi.php
@@ -1,6 +1,7 @@
 <?php
 /** 
  * Gongbi Skin
+ * includes/GongbiTemplate.php
  * © 2015-2021 Isarra
  * © 2021-2022 WaitSpring
  */
