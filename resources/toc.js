5a80cc6fcc499bf3ec9bc1be8e108c3ac3a4f23b
WaitSpring <me@waitspring.com>
2022-06-27 11:55:22 +0800

    fix toc.js: "null" when #toc>ul does not exist.

--- resources/toc.js
+++ resources/toc.js
@@ -16,19 +16,20 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-'use strict';
+ 'use strict';
 
-/* TOC (Left Sidebar) */
-$( function () {
-	if ( $( '#toc' ).length !== 0 ) {
-		var $div = $( '<div>' ).attr( 'id', 'site-toc' ).addClass( 'sidebar-chunk' ),
-			$title = $( '#mw-toc-heading' ).html(),
-			$titleH2 = $( '<h2><span>' + $title + '</span></h2>' ),
-			$titleH3 = $( '<h3 id="p-toc-label">' + $title + '</h2>' ),
-			$divInner = $( '<div>' ).addClass( 'sidebar-inner' ),
-			$divPortlet = $( '<div>' ).attr( 'id', 'p-toc' ).attr( 'role', 'navigation' ).attr( 'aria-labelledby', 'p-toc-label' ).addClass( 'mw-portlet' ),
-			$portletBody = $( '<ul>' ).addClass( 'mw-portlet-body' ),
-			$tocUl = $( '#toc>ul' ).html();
-		$div.append( $titleH2 ).append( $divInner.append( $divPortlet.append( $titleH3 ).append( $portletBody.append( $tocUl ) ) ) ).appendTo( $( '#mw-site-navigation' ) );
+ /* TOC (Left Sidebar) */
+ 
+$(function () {
+	if ($('#toc>ul').length !== 0) {
+		var $div = $('<div>').attr('id', 'site-toc').addClass('sidebar-chunk'),
+			$title = $('#mw-toc-heading').html(),
+			$titleH2 = $('<h2><span>' + $title + '</span></h2>'),
+			$titleH3 = $('<h3 id="p-toc-label">' + $title + '</h2>'),
+			$divInner = $('<div>').addClass('sidebar-inner'),
+			$divPortlet = $('<div>').attr('id', 'p-toc').attr('role', 'navigation').attr('aria-labelledby', 'p-toc-label').addClass('mw-portlet'),
+			$portletBody = $('<ul>').addClass('mw-portlet-body'),
+			$tocUl = $('#toc>ul').html();
+		$div.append($titleH2).append($divInner.append($divPortlet.append($titleH3).append($portletBody.append($tocUl)))).appendTo($('#mw-site-navigation'));
 	}
-} );
+}); 
