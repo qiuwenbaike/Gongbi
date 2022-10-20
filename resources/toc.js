07b14f7b40f6fac07a296c535d659182e875fa40
WaitSpring <me@waitspring.com>
2022-10-20 14:57:27 +0800

    update js

--- resources/toc.js
+++ resources/toc.js
@@ -18,27 +18,28 @@
  */
 'use strict';
 
-/* TOC (Sidebar) */
-
 $( function () {
 	if ( $( '#toc>ul' ).length !== 0 ) {
+		/* TOC (Left) */
 		var $div = $( '<div>' ).attr( 'id', 'site-toc' ).addClass( 'sidebar-chunk' ),
 			$title = $( '#mw-toc-heading' ).html(),
 			$titleH2 = $( '<h2><span>' + $title + '</span></h2>' ),
 			$titleH3 = $( '<h3 id="p-toc-label">' + $title + '</h3>' ),
 			$divInner = $( '<div>' ).addClass( 'sidebar-inner' ),
 			$divPortlet = $( '<div>' ).attr( 'id', 'p-toc' ).attr( 'role', 'navigation' ).attr( 'aria-labelledby', 'p-toc-label' ).addClass( 'mw-portlet' ),
-			$divPortletRight = $( '<div>' ).attr( 'id', 'p-toc-right' ).attr( 'role', 'navigation' ).attr( 'aria-labelledby', 'p-toc-label' ).addClass( 'mw-portlet' ),
 			$portletBody = $( '<ul>' ).addClass( 'mw-portlet-body' ),
-			$tocUl = $( '#toc>ul' ).html(),
-			$divRight = $( '<div>' ).attr( 'id', 'site-toc-right' ).addClass( 'sidebar-chunk' ),
+			$tocUl = $( '#toc>ul' ).html();
+		$div.append( $titleH2 ).append( $divInner.append( $divPortlet.append( $titleH3 ).append( $portletBody.append( $tocUl ) ) ) ).appendTo( $( '#mw-site-navigation' ) );
+
+		/* TOC (Right) */
+		var $divRight = $( '<div>' ).attr( 'id', 'site-toc-right' ).addClass( 'sidebar-chunk' ),
+			$divPortletRight = $( '<div>' ).attr( 'id', 'p-toc-right' ).attr( 'role', 'navigation' ).attr( 'aria-labelledby', 'p-toc-label' ).addClass( 'mw-portlet' ),
 			$titleRight = $( '#mw-toc-heading' ).html(),
 			$titleH2Right = $( '<h2><span>' + $titleRight + '</span></h2>' ),
 			$titleH3Right = $( '<h3 id="p-toc-right-label">' + $titleRight + '</h3>' ),
 			$divInnerRight = $divInner.clone(),
 			$portletBodyRight = $portletBody.clone(),
 			$tocUlRight = $( '#toc>ul' ).html();
-		$div.append( $titleH2 ).append( $divInner.append( $divPortlet.append( $titleH3 ).append( $portletBody.append( $tocUl ) ) ) ).appendTo( $( '#mw-site-navigation' ) );
 		$divRight.append( $titleH2Right ).append( $divInnerRight.append( $divPortletRight.append( $titleH3Right ).append( $portletBodyRight.append( $tocUlRight ) ) ) ).appendTo( $( '#mw-related-navigation' ) );
 	}
 } );
