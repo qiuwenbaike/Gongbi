7577a7d0f69910eeb114fa2eb8eb946b9cc7d395
WaitSpring <me@waitspring.com>
2022-10-20 14:34:43 +0800

    test: toc left

--- resources/toc.js
+++ resources/toc.js
@@ -23,13 +23,17 @@
 $( function () {
 	if ( $( '#toc>ul' ).length !== 0 ) {
 		var $div = $( '<div>' ).attr( 'id', 'site-toc' ).addClass( 'sidebar-chunk' ),
+			$divRight = $( '<div>' ).attr( 'id', 'site-toc-right' ).addClass( 'sidebar-chunk' ),
 			$title = $( '#mw-toc-heading' ).html(),
 			$titleH2 = $( '<h2><span>' + $title + '</span></h2>' ),
-			$titleH3 = $( '<h3 id="p-toc-label">' + $title + '</h2>' ),
+			$titleH3 = $( '<h3 id="p-toc-label">' + $title + '</h3>' ),
+			$titleH3Right = $( '<h3 id="p-toc-right-label">' + $title + '</h3>' ),
 			$divInner = $( '<div>' ).addClass( 'sidebar-inner' ),
 			$divPortlet = $( '<div>' ).attr( 'id', 'p-toc' ).attr( 'role', 'navigation' ).attr( 'aria-labelledby', 'p-toc-label' ).addClass( 'mw-portlet' ),
+			$divPortletRight = $( '<div>' ).attr( 'id', 'p-toc-right' ).attr( 'role', 'navigation' ).attr( 'aria-labelledby', 'p-toc-label' ).addClass( 'mw-portlet' ),
 			$portletBody = $( '<ul>' ).addClass( 'mw-portlet-body' ),
 			$tocUl = $( '#toc>ul' ).html();
 		$div.append( $titleH2 ).append( $divInner.append( $divPortlet.append( $titleH3 ).append( $portletBody.append( $tocUl ) ) ) ).appendTo( $( '#mw-site-navigation' ) );
+		$divRight.append( $titleH2 ).append( $divInner.append( $divPortletRight.append( $titleH3Right ).append( $portletBody.append( $tocUl ) ) ) ).appendTo( $( '#mw-related-navigation' ) );
 	}
 } );
